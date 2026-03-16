e75233191c8c53855bb8020d95c726c5d99faf62
prod-slixk <jayjarratt6@icloud.com>
2026-03-15 21:57:21 -0400

    fixed href links across 3 different files

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -120,9 +120,23 @@ const HotCollections = () => {
                         <button>Buy Now</button>
                         <div className="nft__item_share">
                           <h4>Share</h4>
-                          <a href="" target="_blank" rel="noreferrer"><i className="fa fa-facebook fa-lg"></i></a>
-                          <a href="" target="_blank" rel="noreferrer"><i className="fa fa-twitter fa-lg"></i></a>
-                          <a href=""><i className="fa fa-envelope fa-lg"></i></a>
+                          <button type="button" style={{ background: "none", border: "none", padding: 0, cursor: "pointer" }} aria-label="Share on Facebook">
+                            <i className="fa fa-facebook fa-lg"></i>
+                          </button>
+                          <button
+                            type="button"
+                            style={{ background: "none", border: "none", padding: 0, cursor: "pointer" }}
+                            aria-label="Share on Twitter"
+                          >
+                            <i className="fa fa-twitter fa-lg"></i>
+                          </button>
+                          <button
+                            type="button"
+                            style={{ background: "none", border: "none", padding: 0, cursor: "pointer" }}
+                            aria-label="Share via Email"
+                          >
+                            <i className="fa fa-envelope fa-lg"></i>
+                          </button>
                         </div>
                       </div>
                     </div>
