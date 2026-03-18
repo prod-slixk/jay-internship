648e7307ab84fa0fbda317a7b4690804502d77dc
prod-slixk <jayjarratt6@icloud.com>
2026-03-18 15:35:37 -0400

    fix: dynamic content to disappear after load on the Explore page.

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -40,11 +40,9 @@ const ExploreCard = ({ item }) => {
             <button>Buy Now</button>
             <div className="nft__item_share">
               <h4>Share</h4>
-              <a href="https://facebook.com" target="_blank" rel="noreferrer"><i className="fa fa-facebook fa-lg"></i></a>
-              <a href="https://twitter.com" target="_blank" rel="noreferrer"><i className="fa fa-twitter fa-lg"></i></a>
-              <button type="button" style={{ background: "none", border: "none", padding: 0, cursor: "pointer" }} aria-label="Share via email">
-                <i className="fa fa-envelope fa-lg"></i>
-              </button>
+              <a href="#" target="_blank" rel="noreferrer"><i className="fa fa-facebook fa-lg"></i></a>
+              <a href="#" target="_blank" rel="noreferrer"><i className="fa fa-twitter fa-lg"></i></a>
+              <a href="#"><i className="fa fa-envelope fa-lg"></i></a>
             </div>
           </div>
         </div>
@@ -64,13 +62,13 @@ const ExploreCard = ({ item }) => {
   );
 };
 
-// from main!!! do not remove
 const ExploreItems = () => {
   const { items, loading, error, filterItems, loadMore, hasMore } = useExploreItems();
 
   // Refresh AOS whenever items change so dynamically rendered cards animate in
   useEffect(() => {
-    AOS.refresh();
+    const timer = setTimeout(() => AOS.refresh(), 50);
+    return () => clearTimeout(timer);
   }, [items]);
 
   const handleFilterChange = (e) => {
